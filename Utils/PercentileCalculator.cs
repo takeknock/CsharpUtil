26170ce4039e5a14e0b6ea5074b60b217d002815
mmatthew <takeknock@gmail.com>
2020-06-23 22:11:42 +0900

    feat: add PercentileCalculator

---
+++ Utils/PercentileCalculator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utils
+{
+    class PercentileCalculator
+    {
+        private double[] data;
+        public PercentileCalculator(IEnumerable<double> sequence)
+        {
+            data = sequence.ToArray();
+            Array.Sort(data);
+        }
+
+        /// <summary>
+        /// O(N) because of ToArray as deep copy.
+        /// </summary>
+        /// <param name="sequence"></param>
+        /// <param name="percentile"></param>
+        /// <returns></returns>
+        public double Calculate(double percentile)
+        {
+            int N = data.Length;
+            double n = (N - 1) * percentile + 1;
+            if (n == 1d) return data[0];
+            else if (n == N) return data[N - 1];
+            else
+            {
+                int k = (int)n;
+                double d = n - k;
+                return data[k - 1] + d * (data[k] - data[k - 1]);
+            }
+        }
+    }
+}
---
+++ Utils/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utils
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] data = { 0, 1, 2, 3, 4, 5 };
+            PercentileCalculator percentile = new PercentileCalculator(data);
+            double result = percentile.Calculate(0.25);
+            Console.WriteLine(result);
+
+
+
+        }
+    }
+}
