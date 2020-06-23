b110931a4c428dab4c42c598b49716a3c3718567
mmatthew <takeknock@gmail.com>
2020-06-23 22:23:54 +0900

    feat: DirectoryInfo sample

---
+++ Utils/DirectoryInfoSample.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Utils
+{
+    class DirectoryInfoSample
+    {
+        void Main()
+        {
+            DirectoryInfo di = new DirectoryInfo(@"C:\tests");
+            FileInfo fi = new FileInfo(@"C:\tests\test.txt");
+            if (!di.Exists)
+            {
+                di.Create();
+                fi.Create();
+            }
+            di.Delete(true);
+
+        }
+    }
+}
--- Utils/Program.cs
+++ Utils/Program.cs
@@ -4,18 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.IO;
+
 namespace Utils
 {
     class Program
     {
         static void Main(string[] args)
         {
-            double[] data = { 0, 1, 2, 3, 4, 5 };
-            PercentileCalculator percentile = new PercentileCalculator(data);
-            double result = percentile.Calculate(0.25);
-            Console.WriteLine(result);
+            //double[] data = { 0, 1, 2, 3, 4, 5 };
+            //PercentileCalculator percentile = new PercentileCalculator(data);
+            //double result = percentile.Calculate(0.25);
+            //Console.WriteLine(result);
 
-
+            
 
         }
     }
